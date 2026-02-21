65260325f067e3a75485bf5b7af2441f7ed44acd
Aafrin Shaikh <108276400+aafrin-can-cook@users.noreply.github.com>
2026-02-21 20:01:10 +0000

    Delete SuperBuzz-vite directory

--- SuperBuzz-vite/src/features/home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div className="home-container">
-            <h1>Welcome to SuperBuzz!</h1>
-            <p>This is the home page.</p>
-        </div>
-    );
-}
-export default Home;
--- SuperBuzz-vite/src/features/login/authserv.js
+++
@@ -1,9 +0,0 @@
-export function validateEmail(email) {
-  return email.endsWith("@wlv.ac.uk");
-}
-
-export function validatePassword(password) {
-  const regex =
-    /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/;
-  return regex.test(password);
-}
