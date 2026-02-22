ec85ad486bacea8351d39c83ff23cec3ffd408b7
aafrin-can-cook <dollyshaikh786s@gmail.com>
2026-02-22 10:16:04 +0000

    yada yada

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 // whatever modal id is passed, toggle its visibility
-function toggleModal(id) {
+function hide(id) {
   document.getElementById(id).classList.toggle("hidden");
 }
 
+function show(id) {
+  document.getElementById(id).classList.remove("hidden");
+}
+
+// simple validation functions for email and password
 function validUniversityEmail(email) {
-  return email.endsWith(".ac.uk");
+  return email.endsWith("@wlv.ac.uk");
 }
 
 function validPassword(password) {
-  return password.length >= 8;
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+  return regex.test(password);
 }
 
-// close buttons for all modals
+// close buttons for all modals -> need improvements 
 document.querySelectorAll("[data-close]").forEach(btn => {
   btn.addEventListener("click", () => {
-    toggleModal(btn.getAttribute("data-close"));
+    hide(btn.getAttribute("data-close"));
   });
 });
 
@@ -25,36 +31,63 @@ document.getElementById("login-submit").addEventListener("click", () => {
   const password = document.getElementById("login-password").value;
 
   if (!validUniversityEmail(email)) {
-    alert("Use university email (.ac.uk)");
+    alert("Use university email!");
     return;
   }
 
   if (!validPassword(password)) {
-    alert("Password must be at least 8 characters");
+    alert("Password must be at least 8 characters, include uppercase + lowercase letters and numbers");
     return;
   }
 
   // SUCCESS → Show Main App
-  document.getElementById("login-modal").classList.add("hidden");
-  document.getElementById("main-app").classList.remove("hidden");
+  hide("login-modal");
+  show("main-app");
 });
 
 
 // login side buttons
 document.getElementById("login-to-signup")
   .addEventListener("click", () => {
-    toggleModal("login-modal");
-    toggleModal("signup-modal");
+    hide("login-modal");
+    show("signup-modal");
   });
 
 document.getElementById("forgot-password-btn")
   .addEventListener("click", () => {
-    toggleModal("login-modal");
-    toggleModal("forgot-modal");
+    hide("login-modal");
+    show("forgot-modal");
+  });
+
+
+// Signup
+document.getElementById("signup-submit")
+  .addEventListener("click", () => {
+    const email = document.getElementById("signup-email").value;
+    const password = document.getElementById("signup-password").value;
+
+    if (!validUniversityEmail(email)) {
+      alert("Use university email");
+      return;
+    } 
+    else if (!validPassword(password)) {
+      alert("Password must be 8+ characters, include uppercase + lowercase letters and numbers");
+      return;
+    }
+
+    alert("Signup ready for backend.");
+    hide("signup-modal");
+    show("main-app");
   });
 
 
 // Forgot
+document.getElementById("forgot-reset")
+  .addEventListener("click", () => {
+    hide("forgot-modal");
+    document.getElementById("reset-modal").classList.remove("hidden");
+  });
+
 document.getElementById("forgot-submit")
   .addEventListener("click", () => {
 
@@ -64,25 +97,25 @@ document.getElementById("forgot-submit")
       alert("Use university email");
       return;
     }
-
-    toggleModal("forgot-modal");
-    toggleModal("verify-modal");
   });
 
+document.getElementById("verify-code-submit")
+  .addEventListener("click", () => 
+  {
+    
+    //code should be generated and emailed in real app -> backend work needed
 
-document.getElementById("verify-submit")
-  .addEventListener("click", () => {
-
-    const code = document.getElementById("verify-code").value;
-
-    if (code.length !== 4 || isNaN(code)) {
-      alert("Enter valid 4 digit code");
-      return;
-    }
-
-    toggleModal("verify-modal");
-    toggleModal("reset-modal");
+    //if (code === ) 
+    //{
+      //hide("forgot-modal");
+      //show("main-app");
+    //} 
+    //else 
+    //{
+      //alert("Invalid verification code");
+    //}
   });
+
 
 
 document.getElementById("reset-submit")
@@ -96,5 +129,6 @@ document.getElementById("reset-submit")
     }
 
     alert("Password reset ready for backend.");
-    toggleModal("reset-modal");
+    hide("reset-modal");
+    show("login-modal");
   });
