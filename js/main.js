90f3fae6a43feaecb3b0232880a355b5a686cb7d
aafrin-can-cook <dollyshaikh786s@gmail.com>
2026-03-09 17:34:32 +0000

    DOMContentLoaded check added

--- js/main.js
+++ js/main.js
@@ -35,183 +35,187 @@ function validPassword(password) {
     return regex.test(password);
 }
 
-// close buttons for all modals -> need improvements 
-document.querySelectorAll("[data-close]").forEach(btn => {
-    btn.addEventListener("click", () => {
-        hide(btn.getAttribute("data-close"));
-    });
-});
 
-// LOGIN
-const loginBtn = document.getElementById("login-submit");
-
-loginBtn.addEventListener("click", () => {
-
-    const email = document.getElementById("login-email").value;
-    const password = document.getElementById("login-password").value;
-
-    if (!validUniversityEmail(email)) {
-        alert("Use university email!");
-        return;
-    }
-
-    if (!validPassword(password)) {
-        alert("Password must be at least 8 characters, include uppercase + lowercase letters and numbers");
-        return;
-    }
-
-    // SUCCESS → Show Main App
-    hide("login-modal");
-    show("main-app");
-});
-
-
-// login side buttons
-const signupBtn = document.getElementById("login-to-signup");
-signupBtn
-    .addEventListener("click", () => {
-        hide("login-modal");
-        show("signup-modal");
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // close buttons for all modals -> need improvements 
+    document.querySelectorAll("[data-close]").forEach(btn => {
+        btn.addEventListener("click", () => {
+            hide(btn.getAttribute("data-close"));
+        });
     });
 
-const forgotPasswordBtn = document.getElementById("forgot-password-btn");
-forgotPasswordBtn
-    .addEventListener("click", () => {
-        hide("login-modal");
-        show("forgot-modal");
-    });
+    // LOGIN
+    const loginBtn = document.getElementById("login-submit");
 
+    loginBtn.addEventListener("click", () => {
 
-// Signup
-const signupBtn2 = document.getElementById("signup-submit");
-signupBtn2
-    .addEventListener("click", async () => {
-        const email = document.getElementById("signup-email").value;
-        const password = document.getElementById("signup-password").value;
-
-        await signup(email, password);
-    });
-
-const backBtn = document.getElementById("back");
-backBtn
-    .addEventListener("click", () => {
-        hide("signup-modal");
-        show("login-modal");
-    });
-
-
-// Forgot
-const forgotSubmitBtn = document.getElementById("forgot-submit");
-forgotSubmitBtn
-    .addEventListener("click", () => {
-        hide("forgot-modal");
-        document.getElementById("reset-modal").classList.remove("hidden");
-    });
-
-const forgotSubmitBtn2 = document.getElementById("forgot-submit");
-forgotSubmitBtn2
-    .addEventListener("click", () => {
-
-        const email = document.getElementById("forgot-email").value;
+        const email = document.getElementById("login-email").value;
+        const password = document.getElementById("login-password").value;
 
         if (!validUniversityEmail(email)) {
-            alert("Use university email");
-            return;
-        }
-    });
-
-const backBtn2 = document.getElementById("back2");
-backBtn2
-    .addEventListener("click", () => {
-        hide("forgot-modal");
-        show("login-modal");
-    });
-
-
-const verifyCodeBtn = document.getElementById("verify-code-submit");
-verifyCodeBtn
-    .addEventListener("click", () => {
-
-        //code should be generated and emailed in real app -> backend work needed
-
-        //if (code === ) 
-        //{
-        //hide("forgot-modal");
-        //show("main-app");
-        //} 
-        //else 
-        //{
-        //alert("Invalid verification code");
-        //}
-
-    });
-
-
-const resetSubmitBtn = document.getElementById("reset-submit");
-resetSubmitBtn
-    .addEventListener("click", () => {
-
-        const newPassword = document.getElementById("new-password").value;
-
-        if (!validPassword(newPassword)) {
-            alert("Password must be 8+ characters");
+            alert("Use university email!");
             return;
         }
 
-        alert("Password reset ready for backend.");
-        hide("reset-modal");
-        show("login-modal");
-    });
-
-const backBtn3 = document.getElementById("back3");
-backBtn3
-    .addEventListener("click", () => {
-        hide("reset-modal");
-        show("login-modal");
-    });
-
-// for later use -> ✖
-
-//sidebar and chatbox toggles
-const chatToggleBtn = document.getElementById("chat-toggle");
-chatToggleBtn
-    .addEventListener("click", () => {
-        const element = document.getElementById("chatbox");
-
-        if (element.classList.contains("hidden")) {
-            show("chatbox")
+        if (!validPassword(password)) {
+            alert("Password must be at least 8 characters, include uppercase + lowercase letters and numbers");
+            return;
         }
-        else {
-            hide("chatbox")
-        }
+
+        // SUCCESS → Show Main App
+        hide("login-modal");
+        show("main-app");
     });
 
 
+    // login side buttons
+    const signupBtn = document.getElementById("login-to-signup");
+    signupBtn
+        .addEventListener("click", () => {
+            hide("login-modal");
+            show("signup-modal");
+        });
 
-const sidebarToggleBtn = document.getElementById("sidebar-toggle");
-sidebarToggleBtn
-    .addEventListener("click", () => {
-        const sidebar = document.getElementById("sidebar");
-        sidebar.classList.toggle("-translate-x-full");
-    });
+    const forgotPasswordBtn = document.getElementById("forgot-password-btn");
+    forgotPasswordBtn
+        .addEventListener("click", () => {
+            hide("login-modal");
+            show("forgot-modal");
+        });
+
+
+    // Signup
+    const signupBtn2 = document.getElementById("signup-submit");
+    signupBtn2
+        .addEventListener("click", async () => {
+            const email = document.getElementById("signup-email").value;
+            const password = document.getElementById("signup-password").value;
+
+            await signup(email, password);
+        });
+
+    const backBtn = document.getElementById("back");
+    backBtn
+        .addEventListener("click", () => {
+            hide("signup-modal");
+            show("login-modal");
+        });
+
+
+    // Forgot
+    const forgotSubmitBtn = document.getElementById("forgot-submit");
+    forgotSubmitBtn
+        .addEventListener("click", () => {
+            hide("forgot-modal");
+            document.getElementById("reset-modal").classList.remove("hidden");
+        });
+
+    const forgotSubmitBtn2 = document.getElementById("forgot-submit");
+    forgotSubmitBtn2
+        .addEventListener("click", () => {
+
+            const email = document.getElementById("forgot-email").value;
+
+            if (!validUniversityEmail(email)) {
+                alert("Use university email");
+                return;
+            }
+        });
+
+    const backBtn2 = document.getElementById("back2");
+    backBtn2
+        .addEventListener("click", () => {
+            hide("forgot-modal");
+            show("login-modal");
+        });
+
+
+    const verifyCodeBtn = document.getElementById("verify-code-submit");
+    verifyCodeBtn
+        .addEventListener("click", () => {
+
+            //code should be generated and emailed in real app -> backend work needed
+
+            //if (code === ) 
+            //{
+            //hide("forgot-modal");
+            //show("main-app");
+            //} 
+            //else 
+            //{
+            //alert("Invalid verification code");
+            //}
+
+        });
+
+
+    const resetSubmitBtn = document.getElementById("reset-submit");
+    resetSubmitBtn
+        .addEventListener("click", () => {
+
+            const newPassword = document.getElementById("new-password").value;
+
+            if (!validPassword(newPassword)) {
+                alert("Password must be 8+ characters");
+                return;
+            }
+
+            alert("Password reset ready for backend.");
+            hide("reset-modal");
+            show("login-modal");
+        });
+
+    const backBtn3 = document.getElementById("back3");
+    backBtn3
+        .addEventListener("click", () => {
+            hide("reset-modal");
+            show("login-modal");
+        });
+
+    // for later use -> ✖
+
+    //sidebar and chatbox toggles
+    const chatToggleBtn = document.getElementById("chat-toggle");
+    chatToggleBtn
+        .addEventListener("click", () => {
+            const element = document.getElementById("chatbox");
+
+            if (element.classList.contains("hidden")) {
+                show("chatbox")
+            }
+            else {
+                hide("chatbox")
+            }
+        });
 
 
 
-// bus, gym, library modals
-const busBtn = document.getElementById("bus-timings");    
-busBtn
-    .addEventListener("click", () => {
-        show("bus-modal");
-    });
+    const sidebarToggleBtn = document.getElementById("sidebar-toggle");
+    sidebarToggleBtn
+        .addEventListener("click", () => {
+            const sidebar = document.getElementById("sidebar");
+            sidebar.classList.toggle("-translate-x-full");
+        });
 
-const gymBtn = document.getElementById("gym-timings");
-gymBtn
-    .addEventListener("click", () => {
-        show("gym-modal");
-    });
 
-const libraryBtn = document.getElementById("library-timings");
-libraryBtn
-    .addEventListener("click", () => {
-        show("library-modal");
-    });
+
+    // bus, gym, library modals
+    const busBtn = document.getElementById("bus-timings");    
+    busBtn
+        .addEventListener("click", () => {
+            show("bus-modal");
+        });
+
+    const gymBtn = document.getElementById("gym-timings");
+    gymBtn
+        .addEventListener("click", () => {
+            show("gym-modal");
+        });
+
+    const libraryBtn = document.getElementById("library-timings");
+    libraryBtn
+        .addEventListener("click", () => {
+            show("library-modal");
+        });
+});
