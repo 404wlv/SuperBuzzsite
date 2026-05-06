629de7ed2489de1d1867fcbdca53d715a062ef43
aafrin-can-cook <dollyshaikh786s@gmail.com>
2026-05-06 06:27:42 +0100

    sidebar fixed

--- js/home.js
+++ js/home.js
@@ -280,12 +280,12 @@ document.addEventListener("DOMContentLoaded", async () => {
             isSidebarOpen = !isSidebarOpen
 
             if (isSidebarOpen) {
-                sidebar?.classList.remove("-translate-x-full")
-                eventGrid.classList.toggle("md:ml-72");
+            sidebar.classList.remove("-translate-x-full")
+            eventGrid.classList.remove("md:ml-72")
             } else {
-                sidebar?.classList.add("-translate-x-full")
-                eventGrid.classList.toggle("md:ml-72");
-            }
+            sidebar.classList.add("-translate-x-full")
+            eventGrid.classList.add("md:ml-72")
+        }
         })
     }
 
